a358e53165ebcf6212bcfb322e9c1d3cf32369f1
Danil Zalizchuk <danilmail0110@gmail.com>
2021-05-11 16:40:23 +0300

    Removed comments in test_css.py

--- test_css.py
+++ test_css.py
@@ -16,23 +16,3 @@ alert_text = twitch_po.get_alert_text()
 expected_alert = 'Такого имени пользователя не существует.'
 
 assert alert_text == expected_alert
-
-# browser.get('https://www.twitch.tv/')
-
-# browser.find_element_by_css_selector('[data-a-target="login-button"]').click
-# body = browser.find_element_by_tag_name('body')
-# modalClass = 'ReactModal__Body--open'
-#
-# assert modalClass in body.get_attribute('class')
-
-# usernameInput = browser.find_element_by_css_selector('input#login-username')
-# passwordInput = browser.find_element_by_css_selector('input#password-input')
-
-# usernameInput.send_keys('forbiz375')
-# passwordInput.send_keys('s1mple98Pass')
-# passwordInput.send_keys(Keys.RETURN)
-#
-# expectedError = 'Такого имени пользователя не существует.'
-# errorMessage = browser.find_element_by_css_selector('.server-message-alert strong').text
-#
-# assert errorMessage == expectedError
